amade/screen

support sgr mouse mose
cherry-picked from v4 version

biggest difference is formatting
some type changes due to use of typedefs (ie struct canvas -> Canvas)

Bug: #37206

src/ansi.c
@@ -1174,6 +1174,12 @@ static void DoCSI(Window *win, int c, int intermediate)
 				win->w_mouse = i ? a1 : 0;
 				LMouseMode(&win->w_layer, win->w_mouse);
 				break;
+			/* case 1005:     UTF-8 mouse mode rejected */
+			case 1006:	/* SGR mouse mode */
+				win->w_extmouse = i ? a1 : 0;
+				LExtMouseMode(&win->w_layer, win->w_extmouse);
+				break;
+			/* case 1015:     UXRVT mouse mode rejected */
 			case 2004:	/* bracketed paste mode */
 				win->w_bracketed = i ? true : false;
 				LBracketedPasteMode(&win->w_layer, win->w_bracketed);

greg17477/gnu-screen

src/ansi.c
@@ -1471,6 +1471,12 @@ int c, intermediate;
 	      curr->w_mouse = i ? a1 : 0;
 	      LMouseMode(&curr->w_layer, curr->w_mouse);
 	      break;
+         /* case 1005:     UTF-8 mouse mode rejected */
+	    case 1006:  /* SGR mouse mode */
+		curr->w_extmouse = i ? a1 : 0;
+		LExtMouseMode(&curr->w_layer, curr->w_extmouse);
+                break;
+	 /* case 1015:     UXRVT mouse mode rejected */
 	    }
 	}
       break;